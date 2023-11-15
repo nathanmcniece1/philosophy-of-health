cc75a7dc38d640acae9f2c5f54d9201248396255
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-11-15 14:39:20 +1000

    v1

--- src/app/components/MenuWrapper/MenuWrapper.js
+++ src/app/components/MenuWrapper/MenuWrapper.js
@@ -1,6 +1,6 @@
 // components/MenuWrapper.js
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MenuIcon from '../MenuIcon/MenuIcon';
 import Menu from '../Menu/Menu';
 
@@ -10,6 +10,21 @@ function MenuWrapper() {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
+  useEffect(() => {
+    // When the menu is shown, disable scrolling on the body
+    if (showMenu) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      // When the menu is hidden, enable scrolling
+      document.body.style.overflow = 'auto';
+    }
+
+    // Cleanup function to set overflow back to default when the component unmounts
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [showMenu]); // Only re-run the effect if showMenu changes
 
   return (
     <>
