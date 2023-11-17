ea5b9d82d50c317c7e11a6b5d5ad6e24c4fb23a2
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-11-17 14:43:20 +1000

    v1

--- src/app/page.js
+++ src/app/page.js
@@ -5,27 +5,37 @@ import Introduction from './components/Introduction/Introduction';
 import Health from './components/Health/Health';
 import Life from './components/Life/Life';
 import Ethics from './components/Ethics/Ethics';
-import Impulse from './components/Impulse/Impulse';
+// import Impulse from './components/Impulse/Impulse';
+import Holygrail from './components/Holygrail/Holygrail';
 import Desire from './components/Desire/Desire';
 import MentalHealth from './components/MentalHealth/MentalHealth';
-import Holygrail from './components/Holygrail/Holygrail';
 import './styles.css';
-
 
 function Home() {
   return (
-    <main>
-      <Cover />
-      <Buttons />
-      <Introduction />
-      <Health />
-      <Life />
-      <Ethics />
-      {/* <Impulse /> */}
-      <Holygrail />
-      <Desire />
-      <MentalHealth />
-    </main>
+    <div className="outer-container"> {/* New outer container */}
+      <main className="home-container">
+        <div className="left-column">
+          <Cover />
+          <Buttons />
+        </div>
+        <div className="right-column">
+          <div className="description">
+            <h2 className="description-heading">Description</h2>
+            <p className="description-body">The Philosophy of Health is an exploration of the concept of health. It is an argument for health as a kind of value superstructure, a value which grounds all others. The advancement of this continuum is the advancement of human potential.</p>
+            <br></br>
+          </div>
+          <Introduction />
+          <Health />
+          <Life />
+          <Ethics />
+          {/* <Impulse /> */}
+          <Holygrail />
+          <Desire />
+          <MentalHealth />
+        </div>
+      </main>
+    </div>
   );
 }
 
